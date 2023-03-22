1da9880868dd8480545ffd40ead551b1d0840ac8
qingxuanmaa <emilymqx@gmail.com>
2023-03-22 11:20:44 -0700

    upload to github

---
+++ store/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+	path('', views.store, name="store"),
+	path('cart/', views.cart, name="cart"),
+	path('update_item/', views.updateItem, name="update_item"),
+]
